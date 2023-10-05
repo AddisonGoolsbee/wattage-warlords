d60f7788cca0020ca8075492b5ea9bad953c6d70
Addison Goolsbee <addisongoolsbee@gmail.com>
2023-10-05 15:00:00 -0400

    Implement button -> three lights functionality

--- build/preproc/ctags_target_for_gcc_minus_e.cpp
+++ build/preproc/ctags_target_for_gcc_minus_e.cpp
@@ -1,46 +1,11 @@
 # 1 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
-// #define BUTTON_PIN 33 
-// #define SWITCH_PIN 25
-
-// #define VRX_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRX pin
-// #define VRY_PIN  39 // ESP32 pin GPIO39 (ADC0) connected to VRY pin
-
-// int valueX = 0; // to store the X-axis value
-// int valueY = 0; // to store the Y-axis value
-
-
-// void setup() {
-//   Serial.begin(9600);
-//   // initialize the pushbutton pin as an pull-up input
-//   pinMode(BUTTON_PIN, INPUT_PULLUP);
-//   pinMode(SWITCH_PIN, INPUT_PULLUP);
-
-// }
-
-
-// void loop() {
-//   Serial.print("Button: ");
-//   Serial.print(digitalRead(BUTTON_PIN));
-//   Serial.print("    Switch: ");
-//   Serial.println(digitalRead(SWITCH_PIN));
-
-//   valueX = analogRead(VRX_PIN);
-//   valueY = analogRead(VRY_PIN);
-//   Serial.print("x = ");
-//   Serial.print(valueX);
-//   Serial.print(", y = ");
-//   Serial.println(valueY);
-//   delay(200);
-// }
-
 /*
-* start mode: leds are off, middle color leds are white. Both players hold button to start the game, at which the controller leds count down 3 2 1
-- pins start at 0
-- just one player to start
+TODO
+
+- pins start at 0, just one player to start
 - pushing the button makes the counter increment
 - pusing the button flashes the led that's incrementing
 - counter translates to front 3 leds being lit
-* when counter gets to certain threshhold, game is over. turn leds off, Flash winner leds and then reset
 - add switch: if controller switch != user switch, decrease score instead of increasing it
 - some sort of negative feedback for bad switch and button press
 - controller switch randomly changes every so often
@@ -49,30 +14,115 @@
 - figure out threshholds for color correct and fine tune a lot
 - randomly change the controller joystick color
 - add two users
+
+* start mode: leds are off, middle color leds are white. Both players hold button to start the game, at which the controller leds count down 3 2 1
+* when counter gets to certain threshhold, game is over. turn leds off, Flash winner leds and then reset
+
 */
+# 36 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+int LED_MAX = 192 / 3;
+
+
+int scoreP1 = 0;
+int scoreP2 = 0;
+
+int lastStateP1 = 0x1;
+int currentStateP1;
+int lastSteadyStateP1 = 0x0;
+int lastFlickerableStateP1 = 0x0;
+unsigned long lastDebounceTimeP1 = 0;
+
+int lastStateP2 = 0x1;
+int currentStateP2;
+int lastSteadyStateP2 = 0x0;
+int lastFlickerableStateP2 = 0x0;
+unsigned long lastDebounceTimeP2 = 0;
 
 int ledPin = 32; // The LED is connected to digital pin 9
 int brightness = 0; // Initialize the brightness value
 int fadeAmount = 5; // Rate of brightness change
 
+
+void handleButton(int player){
+  if (player == 1){
+     currentStateP1 = digitalRead(21);
+
+    if (currentStateP1 != lastFlickerableStateP1) {
+      lastDebounceTimeP1 = millis();
+      lastFlickerableStateP1 = currentStateP1;
+    }
+
+    if ((millis() - lastDebounceTimeP1) > 25) {
+      if(lastSteadyStateP1 == 0x1 && currentStateP1 == 0x0) {
+        Serial.print("P1 press");
+        Serial.println(scoreP1);
+        scoreP1++;
+        setCharge(1);
+      }
+      lastSteadyStateP1 = currentStateP1;
+    }
+    lastStateP1 = currentStateP1;
+  } else {
+    currentStateP2 = digitalRead(-1);
+
+    if (currentStateP2 != lastFlickerableStateP2) {
+      lastDebounceTimeP2 = millis();
+      lastFlickerableStateP2 = currentStateP2;
+    }
+
+    if ((millis() - lastDebounceTimeP2) > 25) {
+      if(lastSteadyStateP2 == 0x1 && currentStateP2 == 0x0) {
+        Serial.print("P2 press");
+        Serial.println(scoreP2);
+        scoreP2++;
+      }
+      lastSteadyStateP2 = currentStateP2;
+    }
+    lastStateP2 = currentStateP2;
+  }
+}
+
+void setCharge(int player) {
+  int score = player == 1 ? scoreP1 : scoreP2;
+  int pin = -1;
+
+  Serial.println(score * 4);
+  if (score > LED_MAX * 2) {
+    score = score - (LED_MAX * 2);
+    pin = player == 1 ? 32 : -1;
+  } else if (score > LED_MAX) {
+    score = score - LED_MAX;
+    pin = player == 1 ? 33 : -1;
+  } else {
+    pin = player == 1 ? 25 : -1;
+  }
+
+  analogWrite(pin, score * 4);
+}
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hi");
-  pinMode(ledPin, 0x03); // Set the LED pin as an output
+  pinMode(21, 0x05);
+  pinMode(32, 0x03);
+  pinMode(33, 0x03);
+  pinMode(25, 0x03);
 }
 
 void loop() {
-  Serial.println("Button: ");
+  handleButton(1);
 
-  analogWrite(ledPin, brightness); // Set the LED brightness
-
-  brightness = brightness + fadeAmount; // Increase brightness
-
-  // If brightness reaches the maximum (255), change direction
-  if (brightness == 255 || brightness == 0) {
-    fadeAmount = -fadeAmount; // Change direction
-    delay(500); // Delay for a moment at the maximum and minimum brightness
-  }
-
-  delay(30); // Adjust the delay to control the speed of the oscillation
 }
+
+  // analogWrite(ledPin, brightness); // Set the LED brightness
+
+  // brightness = brightness + fadeAmount; // Increase brightness
+
+  // // If brightness reaches the maximum (255), change direction
+  // if (brightness == 255 || brightness == 0) {
+  //   fadeAmount = -fadeAmount; // Change direction
+  //   delay(500); // Delay for a moment at the maximum and minimum brightness
+  // }
+
+  // delay(5); // Adjust the delay to control the speed of the oscillation
--- build/sketch/led-game.ino.cpp
+++ build/sketch/led-game.ino.cpp
@@ -1,47 +1,12 @@
 #include <Arduino.h>
 #line 1 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
-// #define BUTTON_PIN 33 
-// #define SWITCH_PIN 25
-
-// #define VRX_PIN  36 // ESP32 pin GPIO36 (ADC0) connected to VRX pin
-// #define VRY_PIN  39 // ESP32 pin GPIO39 (ADC0) connected to VRY pin
-
-// int valueX = 0; // to store the X-axis value
-// int valueY = 0; // to store the Y-axis value
-
-
-// void setup() {
-//   Serial.begin(9600);
-//   // initialize the pushbutton pin as an pull-up input
-//   pinMode(BUTTON_PIN, INPUT_PULLUP);
-//   pinMode(SWITCH_PIN, INPUT_PULLUP);
-
-// }
-  
-
-// void loop() {
-//   Serial.print("Button: ");
-//   Serial.print(digitalRead(BUTTON_PIN));
-//   Serial.print("    Switch: ");
-//   Serial.println(digitalRead(SWITCH_PIN));
-
-//   valueX = analogRead(VRX_PIN);
-//   valueY = analogRead(VRY_PIN);
-//   Serial.print("x = ");
-//   Serial.print(valueX);
-//   Serial.print(", y = ");
-//   Serial.println(valueY);
-//   delay(200);
-// }
-
 /*
-* start mode: leds are off, middle color leds are white. Both players hold button to start the game, at which the controller leds count down 3 2 1
-- pins start at 0
-- just one player to start
+TODO
+
+- pins start at 0, just one player to start
 - pushing the button makes the counter increment
 - pusing the button flashes the led that's incrementing
 - counter translates to front 3 leds being lit
-* when counter gets to certain threshhold, game is over. turn leds off, Flash winner leds and then reset
 - add switch: if controller switch != user switch, decrease score instead of increasing it
 - some sort of negative feedback for bad switch and button press
 - controller switch randomly changes every so often
@@ -50,36 +15,140 @@
 - figure out threshholds for color correct and fine tune a lot
 - randomly change the controller joystick color
 - add two users
+
+* start mode: leds are off, middle color leds are white. Both players hold button to start the game, at which the controller leds count down 3 2 1
+* when counter gets to certain threshhold, game is over. turn leds off, Flash winner leds and then reset
+
 */
+
+#define BUTTON_PIN_P1 21
+#define BUTTON_PIN_P2 -1
+
+#define CHARGE_PIN_P1_1 32
+#define CHARGE_PIN_P1_2 33
+#define CHARGE_PIN_P1_3 25
+
+#define CHARGE_PIN_P2_1 -1
+#define CHARGE_PIN_P2_2 -1
+#define CHARGE_PIN_P2_3 -1
+
+#define DEBOUNCE_TIME 25
+#define SCORE_MAX 192
+
+int LED_MAX = SCORE_MAX / 3;
+
+
+int scoreP1 = 0;
+int scoreP2 = 0;
+
+int lastStateP1 = HIGH;
+int currentStateP1; 
+int lastSteadyStateP1 = LOW; 
+int lastFlickerableStateP1 = LOW; 
+unsigned long lastDebounceTimeP1 = 0;  
+
+int lastStateP2 = HIGH;
+int currentStateP2; 
+int lastSteadyStateP2 = LOW; 
+int lastFlickerableStateP2 = LOW; 
+unsigned long lastDebounceTimeP2 = 0;  
 
 int ledPin = 32; // The LED is connected to digital pin 9
 int brightness = 0; // Initialize the brightness value
 int fadeAmount = 5; // Rate of brightness change
 
-#line 57 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+
+#line 59 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+void handleButton(int player);
+#line 98 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+void setCharge(int player);
+#line 117 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
 void setup();
-#line 63 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+#line 126 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
 void loop();
-#line 57 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+#line 59 "/Users/addisongoolsbee/Desktop/class/CPSC-334/interactiveDevices/led-game/led-game.ino"
+void handleButton(int player){
+  if (player == 1){
+     currentStateP1 = digitalRead(BUTTON_PIN_P1);
+
+    if (currentStateP1 != lastFlickerableStateP1) {
+      lastDebounceTimeP1 = millis();
+      lastFlickerableStateP1 = currentStateP1;
+    }
+
+    if ((millis() - lastDebounceTimeP1) > DEBOUNCE_TIME) {
+      if(lastSteadyStateP1 == HIGH && currentStateP1 == LOW) {
+        Serial.print("P1 press");
+        Serial.println(scoreP1);
+        scoreP1++;
+        setCharge(1);
+      }
+      lastSteadyStateP1 = currentStateP1;
+    }
+    lastStateP1 = currentStateP1;
+  } else {
+    currentStateP2 = digitalRead(BUTTON_PIN_P2);
+
+    if (currentStateP2 != lastFlickerableStateP2) {
+      lastDebounceTimeP2 = millis();
+      lastFlickerableStateP2 = currentStateP2;
+    }
+
+    if ((millis() - lastDebounceTimeP2) > DEBOUNCE_TIME) {
+      if(lastSteadyStateP2 == HIGH && currentStateP2 == LOW) {
+        Serial.print("P2 press");
+        Serial.println(scoreP2);
+        scoreP2++;
+      }
+      lastSteadyStateP2 = currentStateP2;
+    }
+    lastStateP2 = currentStateP2;
+  }
+}
+
+void setCharge(int player) {
+  int score = player == 1 ? scoreP1 : scoreP2;
+  int pin = -1;
+
+  Serial.println(score * 4);
+  if (score > LED_MAX * 2) {
+    score = score - (LED_MAX * 2);
+    pin = player == 1 ? CHARGE_PIN_P1_1 : CHARGE_PIN_P2_1;
+  } else if (score > LED_MAX) {
+    score = score - LED_MAX;
+    pin = player == 1 ? CHARGE_PIN_P1_2 : CHARGE_PIN_P2_2;
+  } else {
+    pin = player == 1 ? CHARGE_PIN_P1_3 : CHARGE_PIN_P2_3;
+  }
+
+  analogWrite(pin, score * 4);
+}
+
+
 void setup() {
   Serial.begin(9600);
   Serial.println("Hi");
-  pinMode(ledPin, OUTPUT); // Set the LED pin as an output
+  pinMode(BUTTON_PIN_P1, INPUT_PULLUP);
+  pinMode(CHARGE_PIN_P1_1, OUTPUT);
+  pinMode(CHARGE_PIN_P1_2, OUTPUT);
+  pinMode(CHARGE_PIN_P1_3, OUTPUT);
 }
 
 void loop() {
-  Serial.println("Button: ");
-
-  analogWrite(ledPin, brightness); // Set the LED brightness
-
-  brightness = brightness + fadeAmount; // Increase brightness
-
-  // If brightness reaches the maximum (255), change direction
-  if (brightness == 255 || brightness == 0) {
-    fadeAmount = -fadeAmount; // Change direction
-    delay(500); // Delay for a moment at the maximum and minimum brightness
-  }
-
-  delay(30); // Adjust the delay to control the speed of the oscillation
+  handleButton(1);
+  
 }
+
+  // analogWrite(ledPin, brightness); // Set the LED brightness
+
+  // brightness = brightness + fadeAmount; // Increase brightness
+
+  // // If brightness reaches the maximum (255), change direction
+  // if (brightness == 255 || brightness == 0) {
+  //   fadeAmount = -fadeAmount; // Change direction
+  //   delay(500); // Delay for a moment at the maximum and minimum brightness
+  // }
+
+  // delay(5); // Adjust the delay to control the speed of the oscillation
+
 
